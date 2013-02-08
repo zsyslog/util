50df63001d9b69ed1c03aabe9eb80da1005592c7
zsyslog <jcjj.system@gmail.com>
2013-02-08 11:17:32 -0200

    Update numbers.js
    Number.currency() now called Number.toCurrency() with more options

--- numbers.js
+++ numbers.js
@@ -13,8 +13,18 @@ Number.prototype.safeDivideBy = function(x) {
     }
 }
 
-Number.prototype.currency = function() {
-    return '$' + (parseFloat(this.valueOf()).toFixed(2)).toString();
+Number.prototype.toCurrency = function(c, d, t){
+    // c: decimals fix
+    // d: decimal separator
+    // t: thousands separator
+    var n = this, 
+		c = isNaN(c = Math.abs(c)) ? 2 : c, 
+		d = d == undefined ? "," : d, 
+		t = t == undefined ? "." : t, 
+		s = n < 0 ? "-" : "", 
+		i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "", 
+		j = (j = i.length) > 3 ? j % 3 : 0;
+	return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
 }
 
 Number.prototype.randomMax = function() {
